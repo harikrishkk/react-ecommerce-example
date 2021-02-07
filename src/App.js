e4ec41aff84aa88a803d9c378c70e165c3d222db
hari <harikrishkk@gmail.com>
2021-02-07 14:14:05 -0500

    Class functional example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Person from './components/Person';
+export default class App extends Component {
+  state = {
+    name: 'Hari',
+    job: 'Developer',
+  };
 
-const App = () => {
-  return (
-    <div className="ph3">
-      <h1 className="f6 fw6 ttu tracked">Basic button</h1>
-      <a className="f6 link dim ph3 pv2 mb2 dib white bg-black" href="#0">
-        Button Text
-      </a>
-      <a className="f6 link dim ph3 pv2 mb2 dib white bg-near-black" href="#0">
-        Button Text
-      </a>
-      <a className="f6 link dim ph3 pv2 mb2 dib white bg-dark-gray" href="#0">
-        Button Text
-      </a>
-      <a className="f6 link dim ph3 pv2 mb2 dib white bg-mid-gray" href="#0">
-        Button Text
-      </a>
-    </div>
-  );
-};
+  changeJob = () => {
+    console.log('CLicked button');
+    this.setState({
+      job: 'Tester',
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Person details={this.state} onJobChange={this.changeJob} />
+      </div>
+    );
+  }
+}
 
-export default App;
+// STate is mutable
+// Prop is immutable
